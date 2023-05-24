ac596c1ec9865ba580179f2553150a210b42e714
Lexi Geller <leximoconner@yahoo.com>
2023-05-23 20:57:38 -0400

    cleaned up js, fixed fetch API section, got weather info to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,104 +5,68 @@ var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
 var apiKey = "bc9531820405a218518fa46f5eca8e90";
 
-var formSubmitHandler = function (event) {
+var formSubmitHandler = function(event) {
   event.preventDefault();
 
-  var username = nameInputEl.value.trim();
+  var city = nameInputEl.value.trim();
 
-  if (username) {
-    getUserRepos(username);
+  if (city) {
+    getWeather(city);
 
     repoContainerEl.textContent = '';
     nameInputEl.value = '';
   } else {
-    alert('Please enter a City');
+    alert('Please enter a city');
   }
 };
 
-var buttonClickHandler = function (event) {
-  // `event.target` is a reference to the DOM element of what programming language button was clicked on the page
+/*var buttonClickHandler = function(event) {
   var language = event.target.getAttribute('data-language');
 
-  // If there is no language read from the button, don't attempt to fetch repos
   if (language) {
-    getFeaturedRepos(language);
-
-    repoContainerEl.textContent = '';
+    alert('You clicked the ' + language + ' button');
   }
-};
+};*/
 
-var getUserRepos = function (user) {
-  var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + user + '&appid=' + APIKey';
+var getWeather = function(city) {
+  var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 
   fetch(apiUrl)
-    .then(function (response) {
+    .then(function(response) {
       if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayRepos(data, user);
+        response.json().then(function(data) {
+          displayWeather(data, city);
         });
       } else {
         alert('Error: ' + response.statusText);
       }
     })
-    .catch(function (error) {
-      alert('Unable to connect to GitHub');
+    .catch(function(error) {
+      alert('Unable to connect to OpenWeatherMap');
     });
 };
 
-var getFeaturedRepos = function (language) {
-  // The `q` parameter is what language we want to query, the `+is:featured` flag adds a filter to return only featured repositories
-  // The `sort` parameter will instruct GitHub to respond with all of the repositories in order by the number of issues needing help
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+var displayWeather = function(weatherData, city) {
+  // Clear previous weather data
+  repoContainerEl.innerHTML = '';
 
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
+  var cityNameEl = document.createElement('h4');
+  cityNameEl.textContent = city;
+  repoContainerEl.appendChild(cityNameEl);
 
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
-    return;
-  }
+  var temperatureEl = document.createElement('p');
+  temperatureEl.textContent = 'Temperature: ' + weatherData.main.temp + ' °F';
+  repoContainerEl.appendChild(temperatureEl);
 
-  repoSearchTerm.textContent = searchTerm;
+  // Display additional weather data as needed
 
-  for (var i = 0; i < repos.length; i++) {
-    // The result will be `<github-username>/<github-repository-name>`
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
+  var humidityEl = document.createElement('p');
+  humidityEl.textContent = 'Humidity: ' + weatherData.main.humidity + '%';
+  repoContainerEl.appendChild(humidityEl);
 
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
+  var windSpeedEl = document.createElement('p');
+  windSpeedEl.textContent = 'Wind Speed: ' + weatherData.wind.speed + ' MPH';
+  repoContainerEl.appendChild(windSpeedEl);
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
